efdb535ac959b8b3bba9c48cc82ec3b003974586
Shane <me@shane.cx>
2019-05-01 23:58:50 -0700

    Random matrix gen

--- knightflow.js
+++ knightflow.js
@@ -4,3 +4,26 @@ function writeCellWeight(x, y, weight, context) {
     context.textAlign = "center";
     context.fillText(weight, ((x * 60) - 30), ((y * 60) - 30));
 }
+
+
+function generateArray(){
+
+    var arr = [...Array(10)].map(e => Array(10));
+
+    for(var y = 0; y < 10; y++) {
+        for(var x = 0; x < 10;x++) {
+            randomEvenInt = Math.floor(Math.random() * 15)*2;
+            arr[x][y] = randomEvenInt;
+        }
+    }
+    console.log(arr);
+    return arr;
+}
+
+function printValues(array, context){
+    for(var y = 0; y < 10; y++) {
+        for(var x = 0; x < 10; x++) {
+            writeCellWeight(y+1, x+1, array[x][y], context);
+        }
+    }
+}
